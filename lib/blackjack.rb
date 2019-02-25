cd7f30bbf96182a3199b4489fa2906aca0df8acd
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-25 22:14:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,11 +55,7 @@ end
 
 def runner
   welcome
-  initial_round
-  until card_total > 21
-  hit?(card_total)
+  card_total = initial_round
   display_card_total(card_total)
-end
-end_game
 end
     
